f39ce44f0fb5e4cad4fcead78a89002beabbcbe5
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-06-14 00:55:38 +0700

    chore: Update ExampleB component tests

--- src/components/interfaces/example/B/button.spec.tsx
+++ src/components/interfaces/example/B/button.spec.tsx
@@ -4,38 +4,45 @@ import { FaCrown } from "react-icons/fa";
 import { ContainerExampleB, IconExampleB, TextExampleB } from "./";
 
 const component = (
-  <ContainerExampleB className="example-b-class" style={{ color: "#ff0000" }}>
-    <IconExampleB className="example-b-class" style={{ color: "#ff0000" }}>
-      <FaCrown data-testid="fa-crown-icon" />
+  <ContainerExampleB className="testing-class" id="testing-id" style={{ color: "#ff0000" }}>
+    <IconExampleB className="testing-class" id="testing-id" style={{ color: "#ff0000" }}>
+      <FaCrown data-testid="testing-icon" />
     </IconExampleB>
-    <TextExampleB className="example-b-class" style={{ color: "#ff0000" }}>
-      This is example text
+    <TextExampleB className="testing-class" id="testing-id" style={{ color: "#ff0000" }}>
+      This is testing text
     </TextExampleB>
   </ContainerExampleB>
 );
 
 describe("ExampleB Component Testing", () => {
-  it("ExampleB component should have an icon", () => {
+  it("Should have a id", () => {
     const { getByTestId } = render(component);
-    expect(getByTestId("fa-crown-icon")).toBeInTheDocument();
+    expect(getByTestId("example-b-container")).toHaveAttribute("id", "testing-id");
+    expect(getByTestId("example-b-icon")).toHaveAttribute("id", "testing-id");
+    expect(getByTestId("example-b-text")).toHaveAttribute("id", "testing-id");
   });
 
-  it("ExampleB component should have text", () => {
+  it("Should have text", () => {
     const { getByTestId } = render(component);
-    expect(getByTestId("example-b-text")).toHaveTextContent("This is example text");
+    expect(getByTestId("example-b-text")).toHaveTextContent("This is testing text");
   });
 
-  it("ExampleB component should have a class name example-b-class", () => {
+  it("Should have a class name testing-class", () => {
     const { getByTestId } = render(component);
-    expect(getByTestId("example-b-container")).toHaveClass("example-b-class");
-    expect(getByTestId("example-b-icon")).toHaveClass("example-b-class");
-    expect(getByTestId("example-b-text")).toHaveClass("example-b-class");
+    expect(getByTestId("example-b-container")).toHaveClass("testing-class");
+    expect(getByTestId("example-b-icon")).toHaveClass("testing-class");
+    expect(getByTestId("example-b-text")).toHaveClass("testing-class");
   });
 
-  it("ExampleB component should have a style attribute with value color: #ff0000", () => {
+  it("Should have a style attribute with value color: #ff0000", () => {
     const { getByTestId } = render(component);
     expect(getByTestId("example-b-container")).toHaveStyle("color: #ff0000");
     expect(getByTestId("example-b-icon")).toHaveStyle("color: #ff0000");
     expect(getByTestId("example-b-text")).toHaveStyle("color: #ff0000");
   });
+});
+
+it("Should have an icon", () => {
+  const { getByTestId } = render(component);
+  expect(getByTestId("testing-icon")).toBeInTheDocument();
 });
